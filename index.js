6cff9a3e7e99095afdcceaf138c45ec64812fa9c
Kirtan Modi <modikirtan19@gmail.com>
2024-06-26 18:37:37 +0530

    update index.js for status updated and remove second api call

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ const makePostRequest = async (orderId) => {
     hash: "ddaf7acd673e33ffed033573b20f1327cf229894",
     created_at: 1701827190,
     store_id: process.env.STORE_ID,
-    scope: "store/order/created",
+    scope: "store/order/statusUpdated",
+    // scope: "store/order/created",
     data: {
       type: "order",
       id: orderId,
+      status: {
+        previous_status_id: 0,
+        new_status_id: 11,
+      },
     },
   };
 
@@ -20,8 +25,8 @@ const makePostRequest = async (orderId) => {
     await axios.post(url, body);
     console.log(`First request for order ID ${orderId} completed successfully.`);
 
-    await axios.post(url, body);
-    console.log(`Second request for order ID ${orderId} completed successfully.`);
+    // await axios.post(url, body);
+    // console.log(`Second request for order ID ${orderId} completed successfully.`);
   } catch (error) {
     console.error(`Error occurred for order ID ${orderId}:`, error.message);
   }
